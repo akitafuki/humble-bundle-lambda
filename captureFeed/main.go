cc4aa41cb75bd8f0a70164c422ebd851fda2c738
Jimmie Maggard Jr <akitafuki@gmail.com>
2023-04-27 03:40:00 -0700

    Starting rearchitecture of this project. Adding GH workflow.

--- captureFeed/main.go
+++ captureFeed/main.go
@@ -2,16 +2,12 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"os"
 	"regexp"
 	"time"
 
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/rdsdataservice"
 	"github.com/mmcdole/gofeed"
 )
 
@@ -36,10 +32,6 @@ func HandleLambdaEvent() error {
 
 	r := regexp.MustCompile(`/?\?p=(?P<postid>\d{4,5})`)
 
-	mySession := session.Must(session.NewSession())
-
-	svc := rdsdataservice.New(mySession, aws.NewConfig().WithRegion("us-west-2"))
-
 	for _, item := range feed.Items {
 		c := item.Categories
 		if FindGameBundle(c, "Game Bundle") {
@@ -48,27 +40,6 @@ func HandleLambdaEvent() error {
 
 			s := r.FindStringSubmatch(item.GUID)
 			log.Println(s[1])
-
-			SQLStatement := fmt.Sprintf(`SELECT %s FROM HumbleBundlePosts;`, s[1])
-
-			req, resp := svc.ExecuteStatementRequest(&rdsdataservice.ExecuteStatementInput{
-				Database:    aws.String(getEnvVariable("DB_NAME")),
-				ResourceArn: aws.String(getEnvVariable("DB_RESOURCE_ARN")),
-				SecretArn:   aws.String(getEnvVariable("DB_SECRETS_ARN")),
-				Sql:         aws.String(SQLStatement),
-			})
-
-			err1 := req.Send()
-			if err1 == nil { // resp is now filled
-				fmt.Println("Response:", resp)
-			} else {
-				fmt.Println("error:", err1)
-			}
-
-			if len(resp.Records) == 0 {
-				log.Println("post not found")
-
-			}
 		}
 	}
 
