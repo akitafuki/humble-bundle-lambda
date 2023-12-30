414eab8ea3eb5acf0a26c4f6d385170a13b090df
Jimmie Maggard Jr <jimmie@nestegg.rent>
2023-12-30 01:45:27 -0600

    Trying to fix date

--- captureFeed/main.go
+++ captureFeed/main.go
@@ -15,10 +15,10 @@ import (
 )
 
 type Bundle struct {
-	URL       string    `dynamodbav:"url"`
-	Title     string    `dynamodbav:"title"`
-	CrawledAt time.Time `dynamodbav:"crawledat"`
-	EndDate   time.Time `dynamodbav:"enddate"`
+	URL       string `dynamodbav:"url"`
+	Title     string `dynamodbav:"title"`
+	CrawledAt string `dynamodbav:"crawledat"`
+	EndDate   string `dynamodbav:"enddate"`
 }
 
 func HandleLambdaEvent() error {
@@ -57,8 +57,8 @@ func HandleLambdaEvent() error {
 
 			newBundle.URL = record.(map[string]interface{})["product_url"].(string)
 			newBundle.Title = record.(map[string]interface{})["tile_name"].(string)
-			newBundle.CrawledAt = time.Now()
-			newBundle.EndDate, _ = time.Parse(time.RFC3339, record.(map[string]interface{})["end_date|datetime"].(string))
+			newBundle.CrawledAt = time.Now().Format(time.RFC3339)
+			newBundle.EndDate = record.(map[string]interface{})["end_date|datetime"].(string)
 
 			av, err := dynamodbattribute.MarshalMap(newBundle)
 			if err != nil {
